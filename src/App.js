f73ba3bd3f77aa57b0e7a12346b6faa88627b1ae
Kelly-Kim04 <kellyndu1@gmail.com>
2026-02-18 00:31:15 -0800

    Header, Credits, Footer
    Added responsive credits section with updated header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import Header from './components/Header';
+import Landing from "./components/Landing";
+import Credits from "./components/Credits";
 import Footer from './components/Footer';
 
 function App() {
-  const [ data, setData ] = useState(null);
+  const [ data, setData ] = useState({
+    headline:"Headline headline Headline headline Headline headline Headline"
+  });
   
   useEffect(() => {
 		fetch("<TODO: insert api url here>")
@@ -12,10 +16,11 @@ function App() {
 		.then(res => setData(res.data['article.aml']))
   }, [])
 
-  return data && (
+  return (
     <div className="App">
       <Header/>
-      Hello Daily Bruin!
+      <Landing data={data}/>
+      <Credits data={data}/>
       <Footer/>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,9 @@ import { mediaQueries } from '../shared/config';
 
 
 const Container = styled.div`
-	background-color: black;
+	background-color: #fff;
 	height: auto;
-	color: white;
+	color: #165383;
 	padding: 60px 0 10px 0;
     text-align: center;
 	h1 {
@@ -50,7 +50,7 @@ const Footer = () => {
           <a href="https://www.twitter.com/dailybruin" target="_blank" rel="noreferrer"><img src={twitter} alt=""/></a>
           <a href="https://www.tiktok.com/@dailybruin" target="_blank" rel="noreferrer"><img src={tiktok} alt=""/></a>
           <a href="http://eepurl.com/cFEiZX" target="_blank" rel="noreferrer"><img src={email} alt=""/></a>
-          <p>Built with Suzy’s ♥ in Kerckhoff 118 by [developers]. Designed by [designers].</p>
+          <p>Built with Suzy’s ♥ in Kerckhoff 118.</p>
         </Socials>
     </Container>
     )
